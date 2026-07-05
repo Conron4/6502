3da41dcc6349978d8330d6b54a3db8e628ef530e
Connor A. Hopley <connor.hopley@outlook.com>
2026-07-05 14:16:12 +1000

    Split Status byte

--- main_6502.cxx
+++ main_6502.cxx
@@ -8,9 +8,15 @@ struct CPU{
     WORD PC; // Program Counter
     BYTE SP; // Stack Pointer
     BYTE A, X, Y;  // Accumulator, X Index Register, Y Index Register
-    BYTE P; // Processor Status Register
+    BYTE C : 1; // Carry Flag
+    BYTE Z : 1; // Zero Flag
+    BYTE I : 1; // Interrupt Disable
+    BYTE D : 1; // Decimal Mode
+    BYTE B : 1; // Break Command
+    BYTE V : 1; // Overflow Flag
+    BYTE N : 1; // Negative Flag
     void reset() {
-        
+
     }
         
 };
