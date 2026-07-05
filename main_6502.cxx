67c6c37aafb1719fc1dff082c7f2ea72e98af889
Connor A. Hopley <connor.hopley@outlook.com>
2026-07-05 14:22:09 +1000

    Reset & Memory

--- main_6502.cxx
+++ main_6502.cxx
@@ -3,11 +3,15 @@
 
 using BYTE = unsigned char;
 using WORD = unsigned short;
+using u32 = unsigned int;
 
 struct CPU{
     WORD PC; // Program Counter
     BYTE SP; // Stack Pointer
+    
     BYTE A, X, Y;  // Accumulator, X Index Register, Y Index Register
+    
+    //STATUS REGISTER
     BYTE C : 1; // Carry Flag
     BYTE Z : 1; // Zero Flag
     BYTE I : 1; // Interrupt Disable
@@ -15,10 +19,19 @@ struct CPU{
     BYTE B : 1; // Break Command
     BYTE V : 1; // Overflow Flag
     BYTE N : 1; // Negative Flag
+    
     void reset() {
-
+        PC = 0xFFFC; // Reset vector address
+        SP = 0x0100; // Stack Pointer initialized to 0x0100
+        A = X = Y = 0; // Clear registers
+        D = C = Z = I = B = V = N = 0; // Clear status flags
     }
         
+};
+
+struct mem {
+    static const u32 MAX_MEM = 1024 * 64;
+    BYTE data[MAX_MEM];
 };
 
 int main() {
