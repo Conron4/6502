7a3178218f7c4332cb3ae5bc058365ad8b0984bd
Connor A. Hopley <150775653+Conron4@users.noreply.github.com>
2026-09-21 09:53:08 +1000

    Cleanup

--- src/opcode_tests.cxx
+++ src/opcode_tests.cxx
@@ -16,10 +16,8 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <iostream>
-#include <iomanip>
 #include <sstream>
 #include <string>
-#include <vector>
 
 // Pull emulator implementation into this test translation unit.
 #define main emulator_entrypoint_for_tests
